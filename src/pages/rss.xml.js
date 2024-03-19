80bde9aab2334089596cb9b76f86a9d0bfd62960
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2024-03-19 20:18:20 +0100

    Final RSS Fixes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,14 +2,18 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+
+	const posts = (await getCollection("blog"))
+		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
 	return rss({
 		title: "Structs & Company | Yusuf Bouzekri",
 		description: "A hacker's best friend",
+		stylesheet: "/styles.xsl",
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/posts/${post.slug}/`,
+			author: "yusufbouzekri@gmail.com (Yusuf Bouzekri)"
 		})),
 	});
 }
